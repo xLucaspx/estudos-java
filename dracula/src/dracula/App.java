114d2953405545ec1db45c6a74e0f5cac97faab7
xLucaspx <xlucaspx@gmail.com>
2022-10-10 15:18:31 -0300

    Changes in dracula theme for visualg

--- dracula/src/dracula/App.java
+++ dracula/src/dracula/App.java
@@ -7,97 +7,38 @@ import java.util.ArrayList;
 
 public class App {
 
-	public static void main(String[] args) throws IOException {
-		ArrayList<Integer> comments = new ArrayList<>();
-		comments.add(78);
-		comments.add(92);
-		comments.add(135);
+	public static void main(String[] args) {
+		String tableLine = String.format("|%25s|%19s|", "", "").replace(' ', '-');
+		String cornerLine = String.format("*%25s*%19s*", "", "").replace(' ', '-');
 
-		ArrayList<Integer> strings = new ArrayList<>();
-		strings.add(255);
-		strings.add(139);
-		strings.add(11);
+		ArrayList<String> table = new ArrayList<>();
 
-		ArrayList<Integer> numbersAndText = new ArrayList<>();
-		numbersAndText.add(255);
-		numbersAndText.add(32);
-		numbersAndText.add(160);
+		table.add(String.format("| %23s |  (%3s, %3s, %3s)  |", "Elemento", "R", "G", "B"));
+		table.add(String.format("| %23s |  (%3d, %3d, %3d)  |", "Comentários", 78, 92, 135));
+		table.add(String.format("| %23s |  (%3d, %3d, %3d)  |", "Constantes caracter", 255, 139, 11));
+		table.add(String.format("| %23s |  (%3d, %3d, %3d)  |", "Constantes numéricas", 255, 106, 128));
+		table.add(String.format("| %23s |  (%3d, %3d, %3d)  |", "Fundo do editor", 45, 47, 62));
+		table.add(String.format("| %23s |  (%3d, %3d, %3d)  |", "Palavras-chave", 53, 129, 255));
+		table.add(String.format("| %23s |  (%3d, %3d, %3d)  |", "Palavras especiais", 28, 249, 83));
+		table.add(String.format("| %23s |  (%3d, %3d, %3d)  |", "Tipos de dados", 46, 216, 252));
+		table.add(String.format("| %23s |  (%3d, %3d, %3d)  |", "Texto em geral", 255, 32, 160));
 
-		ArrayList<Integer> background = new ArrayList<>();
-		background.add(45);
-		background.add(47);
-		background.add(62);
+		try (PrintStream ps = new PrintStream(new File("dracula-colours-for-visualg.txt"))) {
+			ps.println("Drácula para visualG3 \n");
+			ps.println("Cores em RGB para alterar em \"Manutenção\" -> \"Configurações\":\n");
+			ps.println(cornerLine);
 
-		ArrayList<Integer> keyWords = new ArrayList<>();
-		keyWords.add(142);
-		keyWords.add(73);
-		keyWords.add(245);
+			for (int i = 0; i < (table.size() - 1); i++) {
+				ps.println(table.get(i));
+				ps.println(tableLine);
+			}
 
-		ArrayList<Integer> specialWords = new ArrayList<>();
-		specialWords.add(28);
-		specialWords.add(249);
-		specialWords.add(83);
+			ps.println(table.get(table.size() - 1));
+			ps.println(cornerLine);
+			ps.println("\nAproveite!");
 
-		ArrayList<Integer> dataTypes = new ArrayList<>();
-		dataTypes.add(46);
-		dataTypes.add(216);
-		dataTypes.add(252);
-
-		String tableLine = String.format("|%22s|%22s|", "", "").replace(' ', '-');
-		String cornerLine = String.format("*%22s*%22s*", "", "").replace(' ', '-');
-
-		ArrayList<String> texto = new ArrayList<>();
-		
-		String text0 = "Drácula para visualG3 \n";
-		String text1 = "Cores em RGB para alterar em \"Manutenção\" -> \"Configurações\":\n";
-		String text2 = cornerLine;
-		String text3 = String.format("| %20s | %20s |", "Elemento", "[R, G, B]");
-		String text4 = tableLine;
-		String text5 = String.format("| %20s | %20s |", "Comentários", comments);
-		String text6 = tableLine;
-		String text7 = String.format("| %20s | %20s |", "Constantes caracter", strings);
-		String text8 = tableLine;
-		String text9 = String.format("| %20s | %20s |", "Constantes numéricas", numbersAndText);
-		String text10 = tableLine;
-		String text11 = String.format("| %20s | %20s |", "Fundo do editor", background);
-		String text12 = tableLine;
-		String text13 = String.format("| %20s | %20s |", "Palavras-chave", keyWords);
-		String text14 = tableLine;
-		String text15 = String.format("| %20s | %20s |", "Palavras especiais", specialWords);
-		String text16 = tableLine;
-		String text17 = String.format("| %20s | %20s |", "Tipos de dados", dataTypes);
-		String text18 = tableLine;
-		String text19 = String.format("| %20s | %20s |", "Texto em geral", numbersAndText);
-		String text20 = cornerLine;
-		String text21 = "\nAproveite!";
-		
-		texto.add(text0);
-		texto.add(text1);
-		texto.add(text2);
-		texto.add(text3);
-		texto.add(text4);
-		texto.add(text5);
-		texto.add(text6);
-		texto.add(text7);
-		texto.add(text8);
-		texto.add(text9);
-		texto.add(text10);
-		texto.add(text11);
-		texto.add(text12);
-		texto.add(text13);
-		texto.add(text14);
-		texto.add(text15);
-		texto.add(text16);
-		texto.add(text17);
-		texto.add(text18);
-		texto.add(text19);
-		texto.add(text20);
-		texto.add(text21);
-		
-		PrintStream ps = new PrintStream(new File("dracula-colours-for-visualg.txt"));
-		
-		texto.forEach(text -> ps.println(text));
-
-		ps.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
