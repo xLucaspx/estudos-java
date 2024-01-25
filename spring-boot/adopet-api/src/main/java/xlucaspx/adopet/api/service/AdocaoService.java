384458725c26b4e8d716635676432abaff2d90a4
Lucas da Paz <xlucaspx@gmail.com>
2024-01-25 10:44:56 -0300

    adopet: extraindo logica de email para EmailService

--- spring-boot/adopet-api/src/main/java/xlucaspx/adopet/api/service/AdocaoService.java
+++ spring-boot/adopet-api/src/main/java/xlucaspx/adopet/api/service/AdocaoService.java
@@ -5,8 +5,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import xlucaspx.adopet.api.exception.ValidacaoException;
@@ -20,21 +18,21 @@ public class AdocaoService {
 	@Autowired
 	private AdocaoRepository repository;
 	@Autowired
-	JavaMailSender emailSender;
+	private EmailService emailService;
 
 	public void solicitaAdocao(Adocao adocao) {
 		if (adocao.getPet().getAdotado() == true) throw new ValidacaoException("Pet já foi adotado!");
 
 		List<Adocao> adocoes = repository.findAll();
 		int contador = 0;
-		
+
 		for (Adocao a : adocoes) {
 			if (a.getTutor() == adocao.getTutor() && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO)
 				throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
-			
+
 			if (a.getPet() == adocao.getPet() && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO)
 				throw new ValidacaoException("Pet já está aguardando avaliação para ser adotado!");
-			
+
 			if (a.getTutor() == adocao.getTutor() && a.getStatus() == StatusAdocao.APROVADO) contador++;
 
 			if (contador == 5) throw new ValidacaoException("Tutor chegou ao limite máximo de 5 adoções!");
@@ -44,43 +42,48 @@ public class AdocaoService {
 		adocao.setStatus(StatusAdocao.AGUARDANDO_AVALIACAO);
 		repository.save(adocao);
 
-		SimpleMailMessage email = new SimpleMailMessage();
-		email.setFrom("adopet@email.com.br");
-		email.setTo(adocao.getPet().getAbrigo().getEmail());
-		email.setSubject("Solicitação de adoção");
-		email.setText("Olá " + adocao.getPet().getAbrigo().getNome()
-				+ "!\n\nUma solicitação de adoção foi registrada hoje para o pet: " + adocao.getPet().getNome()
-				+ ". \nFavor avaliar para aprovação ou reprovação.");
-		emailSender.send(email);
+		var emailAbrigo = adocao.getPet().getAbrigo().getEmail();
+		var assunto = "Solicitação de adoção";
+		var mensagem = """
+			Olá %s,\n
+			Uma solicitação de adoção foi registrada hoje para o pet %s.
+			Favor avaliar para aprovação ou reprovação.
+			""".formatted(adocao.getPet().getAbrigo().getNome(), adocao.getPet().getNome());
+
+		emailService.enviaEmail(emailAbrigo, assunto, mensagem);
 	}
 
 	public void aprovaAdocao(Adocao adocao) {
 		adocao.setStatus(StatusAdocao.APROVADO);
 		repository.save(adocao);
 
-		SimpleMailMessage email = new SimpleMailMessage();
-		email.setFrom("adopet@email.com.br");
-		email.setTo(adocao.getTutor().getEmail());
-		email.setSubject("Adoção aprovada");
-		email.setText("Parabéns " + adocao.getTutor().getNome() + "!\n\nSua adoção do pet " + adocao.getPet().getNome()
-				+ ", solicitada em " + adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))
-				+ ", foi aprovada.\nFavor entrar em contato com o abrigo " + adocao.getPet().getAbrigo().getNome()
-				+ " para agendar a busca do seu pet.");
-		emailSender.send(email);
+		var emailTutor = adocao.getTutor().getEmail();
+		var assunto = "Adoção aprovada";
+		var mensagem = """
+			Parabéns %s!\n
+			Sua adoção do pet %s, solicitada em %s, foi aprovada.
+			Favor entrar em contato com o abrigo %s para agendar a busca do seu pet.
+			""".formatted(adocao.getTutor().getNome(), adocao.getPet().getNome(),
+				adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")),
+				adocao.getPet().getAbrigo().getNome());
+
+		emailService.enviaEmail(emailTutor, assunto, mensagem);
 	}
 
 	public void reprovaAdocao(Adocao adocao) {
 		adocao.setStatus(StatusAdocao.REPROVADO);
 		repository.save(adocao);
 
-		SimpleMailMessage email = new SimpleMailMessage();
-		email.setFrom("adopet@email.com.br");
-		email.setTo(adocao.getTutor().getEmail());
-		email.setSubject("Adoção reprovada");
-		email.setText("Olá " + adocao.getTutor().getNome() + "!\n\nInfelizmente sua adoção do pet "
-				+ adocao.getPet().getNome() + ", solicitada em "
-				+ adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) + ", foi reprovada pelo abrigo "
-				+ adocao.getPet().getAbrigo().getNome() + " com a seguinte justificativa: " + adocao.getJustificativaStatus());
-		emailSender.send(email);
+		var emailTutor = adocao.getTutor().getEmail();
+		var assunto = "Adoção reprovada";
+		var mensagem = """
+			Olá %s,\n
+			Infelizmente sua adoção do pet %s, solicitada em %s, foi reprovada pelo abrigo %s com a seguinte justificativa:
+			"%s"
+			""".formatted(adocao.getTutor().getNome(), adocao.getPet().getNome(),
+				adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")),
+				adocao.getPet().getAbrigo().getNome(), adocao.getJustificativaStatus());
+
+		emailService.enviaEmail(emailTutor, assunto, mensagem);
 	}
 }
